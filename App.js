978cab36e1b73aef6f7074a8ed4e3b9462c3a024
Milad Farazian <mfarazia@ucsc.edu>
2020-09-06 20:44:58 -0700

    Update React.js

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Text Clicked");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello
-        There,fjalkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkasslfjoasjofjoijasofjaoisjfoijsaofjoiasjfoiasjfoijasoifjioassjfojasoifjaosisjfoiajfojaofijasosifjaojfajfoasisif
-      </Text>
+      <Text>Hello React Native</Text>
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -19,6 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
