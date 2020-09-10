d439c8a99a544b7a46ce4d3afa59abd62e206e70
Milad Farazian <mfarazia@ucsc.edu>
2020-09-10 14:55:48 -0700

    Update React.js

--- App.js
+++ App.js
@@ -11,6 +11,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    /*justifyContent: "center",*/
+    justifyContent: "center",
   },
 });
